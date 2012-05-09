74ec9949713ef3396a31b38b374ad6b72958319f
Emmanouil Kiagias <emmanouil.kiagias@cern.ch>
2012-05-09 14:49:10 +0200

    clean up the project(keep only whats needed)

--- django_apps/settings.py
+++ django_apps/settings.py
@@ -126,9 +126,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    #'shibsso',
-    'algo',
-    'genplot',
     'generic',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
--- django_apps/urls.py
+++ django_apps/urls.py
@@ -3,19 +3,6 @@ from django.views.generic import DetailView, ListView
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('algo.views',
-    url(r'^algos/$','compare_page'),
-    url(r'^algos/results','compare_page_results'),
-    url(r'^algos/history','history'),
-    url(r'algos/wut', 'dictTest')
-)
-
-urlpatterns += patterns('genplot.views',
-    url(r'^genplot/$','index'),
-    url(r'^genplot/results/$','choose'),
-    url(r'^genplot/service/$','handleService'),
-)
-
 urlpatterns += patterns('generic.views',
     url(r'^generic/$','index'),
     url(r'^generic/results/$','choose'),
@@ -25,9 +12,3 @@ urlpatterns += patterns('generic.views',
 urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
-
-
-###########################################################
-#url(r'^add_data/','algo.views.add'),
-#url(r'^save_file/$','algo.views.save_file'),
-#url(r'^algos/(?P<page_num>\d+)/$','algo.views.prints_num')
