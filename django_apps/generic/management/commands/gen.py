5309762b72a541b9fc49d65be7e7c4d567f4606a
Emmanouil Kiagias <emmanouil.kiagias@cern.ch>
2012-05-09 15:02:54 +0200

    some more cleaning to the project

--- django_apps/generic/management/commands/gen.py
+++ django_apps/generic/management/commands/gen.py
@@ -1,6 +1,6 @@
 from xml.etree.ElementTree import ElementTree
 from django.core.management.base import BaseCommand, CommandError
-from database_test.genplot.models import Job, Histos
+from django_apps.generic.models import App, Attr, AppDes, Blob, AppAtr 
 import json
 
 
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """ 
         Usage :
-            type> python manage.py gen path_to_input_file  
+            type> python manage.py gen2 path_to_input_file  
         """
         myDataDict = {}
         try:
@@ -32,38 +32,36 @@ class Command(BaseCommand):
         try:
             gauss_version = myDataDict['gaussVersion']
             pythia_version = myDataDict['pythiaVersion']
+            ROOT_version = myDataDict['ROOT_Version']
+            eventType = myDataDict['eventType']
             
             if gauss_version == None:
                 gauss_version = 'v31r1'
             if pythia_version == None:
                 pythia_version = "6.424.2"
+                
+            application, created = App.objects.get_or_create(appName='Gauss',appVersion=gauss_version)     
+            appDescription, created = AppDes.objects.get_or_create(app = application,options=eventType) 
             
-                      
-            jobgauss = Job(
-                eventType = myDataDict['eventType'], 
-                gaussVersion = gauss_version, 
-                pythiaVersion = pythia_version, 
-                totalCrossSection = float(myDataDict['totalCrossSection']), 
-                bCrossSection = float(myDataDict['bCrossSection']), 
-                cCrossSection = float(myDataDict['cCrossSection']), 
-                promptCharmCrossSection = float(myDataDict['promptCharmCrossSection']),
-                totalAcceptedEvents = int(myDataDict['totalAcceptedEvents']), 
-                signalProcessCrossSection = float(myDataDict['signalProcessCrossSection']), 
-                signalProcessFromBCrossSection = float(myDataDict['signalProcessFromBCrossSection']), 
-                generatorLevelCutEfficiency = float(myDataDict['generatorLevelCutEfficiency']) , 
-                timePerEvent = float(myDataDict['timePerEvent']))
             
-            jobgauss.save()
+            for key, value in myDataDict.items():
+                if not key == 'gaussVersion' and not key == 'pythiaVersion' and not key == 'histograms' and not key == 'eventType':
+                    atr, created = Attr.objects.get_or_create(name=key, type='string')
+                    appAtr = AppAtr(appDes = appDescription, attr=atr, value=value)
+                    appAtr.save()
+            atr, created = Attr.objects.get_or_create(name='pythiaVersion', type='string')
+            appAtr = AppAtr(appDes = appDescription, attr=atr , value = pythia_version )
+            appAtr.save()
             
             histograms = {}
             histograms = myDataDict['histograms']
             
             for key, value in histograms.items():
-                hist = Histos(job = jobgauss, name = key, data = value)
-                hist.save()
+                blobb = Blob(appDes = appDescription, name = key, data = value, rootVersion=ROOT_version)
+                blobb.save()
             
         except KeyError:
             self.stdout.write('Attributes given in json file are wrong, aborting...\n')
             
         
-        self.stdout.write('Json file for Job added successfully\n')
+        self.stdout.write('Json file for generic Job added successfully\n')
