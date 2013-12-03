c7777d67f81fe7b2b4b3a4ed6c965506983130e9
Stefan Lohn <stefan.lohn@cern.ch>
2013-12-03 13:10:51 +0100

    Formatting query builder in trend anal. Adding att.descr.

--- django_apps/analysis/trend/query_builder.py
+++ django_apps/analysis/trend/query_builder.py
@@ -1,14 +1,36 @@
 def get_queries(requestData, app_name):
-    select_statements = [ 'opt.description as OPTIONS', 'plat.cmtconfig as PLATFORM' ]
-    group_statements = ['apl.appversion' , 'opt.description', 'plat.cmtconfig']
-    from_statements = [ 'lhcbpr_job j', 'lhcbpr_jobresults r', 'lhcbpr_jobattribute att', 
-                   'lhcbpr_platform plat',  'lhcbpr_jobdescription jobdes', 
-                   'lhcbpr_application apl', 'lhcbpr_options opt'  ]
-    where_statements = [ 'j.id = r.job_id',  'j.jobdescription_id = jobdes.id', 'jobdes.application_id = apl.id',
-                    'jobdes.options_id = opt.id', 'r.jobattribute_id = att.id', 
-                    'rf.jobresults_ptr_id = r.id', 'j.platform_id = plat.id' , 'j.success = 1' ]
+    select_statements = [
+        'opt.description as OPTIONS',
+        'plat.cmtconfig as PLATFORM',
+        'att.description'
+    ]
+    group_statements = [
+        'apl.appversion',
+        'opt.description',
+        'plat.cmtconfig',
+        'att.description'
+    ]
+    from_statements = [
+        'lhcbpr_job j',
+        'lhcbpr_jobresults r',
+        'lhcbpr_jobattribute att',
+        'lhcbpr_platform plat',
+        'lhcbpr_jobdescription jobdes',
+        'lhcbpr_application apl',
+        'lhcbpr_options opt',
+        'lhcbpr_resultfloat rf'
+    ]
+    where_statements = [
+        'j.id = r.job_id',
+        'j.jobdescription_id = jobdes.id',
+        'jobdes.application_id = apl.id',
+        'jobdes.options_id = opt.id',
+        'r.jobattribute_id = att.id', 
+        'rf.jobresults_ptr_id = r.id',
+        'j.platform_id = plat.id',
+        'j.success = 1'
+    ]
     
-    from_statements.append('lhcbpr_resultfloat rf')
     
     secondpart_query = ""
     use_host = False
@@ -32,12 +54,18 @@ def get_queries(requestData, app_name):
         group_statements.append('h.hostname')
         from_statements.append('lhcbpr_host h')
         where_statements.append('j.host_id = h.id')
-        
-    query_results = 'select '+' , '.join(select_statements)
-    query_results+=' ,apl.appversion as VERSION , ROUND(AVG(rf.data), 4) AS AVERAGE, ROUND(STDDEV(rf.data), 4) AS STDDEV, count(*) as ENTRIES from '+' , '.join(from_statements)+' where '+' and '.join(where_statements)
     
-    query_groups = 'select distinct '+' , '.join(select_statements)+' from '+' , '.join(from_statements)+' where '+' and '.join(where_statements)
-              
+    select_statements += [
+        'apl.appversion as VERSION',
+        'ROUND(AVG(rf.data), 4) AS AVERAGE',
+        'ROUND(STDDEV(rf.data), 4) AS STDDEV',
+        'count(*) as ENTRIES'
+    ]
+
+    query  = 'select ' + ' , '.join(select_statements)
+    query += ' from  ' + ' , '.join(from_statements) 
+    query += ' where ' + ' and '.join(where_statements)
+    
     options = requestData['options'].split(',')
     if not options[0] == "":
         options_temp = []
@@ -45,6 +73,7 @@ def get_queries(requestData, app_name):
             options_temp.append("opt.id = {0}".format(opt))
         
         secondpart_query += ' AND ( '+' OR '.join(options_temp)+' )'
+
     versions = requestData['versions'].split(',')
     if not versions[0] == "":
         versions_temp = []
@@ -52,6 +81,7 @@ def get_queries(requestData, app_name):
             versions_temp.append("apl.id = {0}".format(ver))
         
         secondpart_query += ' AND ( '+' OR '.join(versions_temp)+' )'
+
     platforms = requestData['platforms'].split(',')
     if not platforms[0] == "":
         platforms_temp = []
@@ -65,12 +95,10 @@ def get_queries(requestData, app_name):
     application_attribute= "  and att.name = \'{0}\'".format(requestData['atr'].split(',')[0])
     if versions[0] == "":
         application_attribute+= " and apl.appname='{0}'".format(app_name)
-    query_results += application_attribute
-    query_groups += application_attribute
+    query += application_attribute
     
     #add the second part of the query which contains the filtering
-    query_results += secondpart_query
-    query_results +=' GROUP BY '+' , '.join(group_statements)
-    query_groups  += secondpart_query
+    query += secondpart_query
+    query +=' GROUP BY '+' , '.join(group_statements)
     
-    return query_groups, query_results
+    return query
