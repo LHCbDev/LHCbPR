668b4964c420537ee1a8f0db4b3366fbb2a064f8
Ben Couturier <ben.couturier@cern.ch>
2014-09-09 19:06:26 +0200

    First version of Valgrind Memcheck analysis

---
+++ django_apps/analysis/Valgrind/__init__.py
@@ -0,0 +1,117 @@
+"""
+
+Brunel report
+
+"""
+
+
+title = 'Brunel Report'
+
+import json
+from django.db import connection
+from django.http import Http404
+from lhcbPR.models import  Application, JobDescription, Job
+from lhcbPR.models import Options, JobResults, JobAttribute, ResultFloat, ResultFile
+from django.db.models import Q
+
+#
+# Projects for which to display this analysis
+#
+################################################################################
+def isAvailableFor(app_name):
+    ''' Available for all applications, this is teh Valgrind memory check '''
+    return True
+
+
+#
+# render method 
+#
+################################################################################
+def render(**kwargs):
+    """From the url is takes the requested application(app_name) , example:
+    /django/lhcbPR/jobDescriptions/BRUNEL ==> app_name = 'BRUNEL' 
+    and depending on the app_name it returns the available versions, options, setupprojects"""
+    app_name = kwargs['app_name']
+
+    apps = Application.objects.filter(appName__exact=app_name)
+    if not apps:
+        return Http404     
+
+    # returning our custom template
+    # Without the options version
+    return { 'template':'analysis/Valgrind/render.html'}
+
+
+def analyse(**kwargs):
+    """ Analyze the brunel versions 
+    """
+
+    #print kwargs
+
+    requestData = kwargs['requestData']
+    app_name = kwargs['app_name']
+    
+    # Result dictionary
+    res = dict()
+    appInfo = []
+    
+    # Now looking up the Brunel Version objects
+    versions = requestData['versions'].split(",")
+    options = requestData['options'].split(",")
+    appversions = [Application.objects.get(pk=k) for k in versions]
+
+    # JobDescription objects
+    for optionId in options:
+        for av in appversions:
+            info = getMemcheckInfo(av, optionId)
+            appInfo.append(info)
+
+    res['results'] = appInfo
+    return res
+
+
+def getMemcheckInfo(app, optionId):
+    '''
+    Returns the information from the Brunel jobs for an application
+    '''
+    appInfo = {}
+
+
+    
+    # Looking up options info
+    opt = Options.objects.get(pk=optionId)
+    
+    # Now getting the Job Description
+    jd = JobDescription.objects.get(options=optionId, application=app)
+    jobs = Job.objects.filter(jobDescription=jd)
+    alljobs = []
+    for j in jobs:
+        jinfo = {}
+        jinfo['id'] = j.id
+        jinfo['time_start'] = j.time_start 
+        jinfo['time_end'] = j.time_end
+        jres = []
+        for jr in JobResults.objects.filter(job=j):
+            if jr.jobAttribute.group != "Valgrind":
+                continue
+            jrestmp = {}
+            jrestmp['name'] = jr.jobAttribute.name
+            jrestmp['type'] = jr.jobAttribute.type
+            jrestmp['group'] = jr.jobAttribute.group
+
+            if  jr.jobAttribute.type == "Float":
+                rf = ResultFloat.objects.get(pk=jr.id)
+                jrestmp['value'] = rf.data
+            if  jr.jobAttribute.type == "File":
+                rf = ResultFile.objects.get(pk=jr.id)
+                jrestmp['value'] =  rf.file
+            jres.append(jrestmp)
+        jinfo['jobresults'] = jres
+        alljobs.append(jinfo)
+      
+    appInfo['jobs'] = alljobs
+    appInfo['version'] = app.appVersion
+    appInfo['application'] = app.appName
+    appInfo['options'] = opt.description
+    print "<", appInfo, ">"
+    return appInfo
